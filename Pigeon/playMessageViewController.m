e9a3d7de364ee110870c4ab719e0e16e60b57cd4
fuzzymath007 <mchess@umich.edu>
2014-07-15 23:22:34 -0400

    Added get data to play Message controller

--- Pigeon/playMessageViewController.m
+++ Pigeon/playMessageViewController.m
@@ -8,6 +8,7 @@
 
 #import "playMessageViewController.h"
 
+
 @interface playMessageViewController ()
 
 @end
@@ -20,21 +21,69 @@
     
     PFFile *audioFile = [self.message objectForKey:@"file"];
     
-    NSData *audioFileData = [audioFile getData];
+    NSLog(@"%@",audioFile);
     
-    NSLog(@"%@",audioFileData);
+    NSString *docsDir;
+    NSArray *dirPaths;
+    
+    dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    docsDir = [dirPaths objectAtIndex:0];
+    NSString *databasePath = [[NSString alloc] initWithString: [docsDir stringByAppendingPathComponent:@"recievedMessage.caf"]];
+    NSURL *audioURL = [[NSURL alloc] initWithString:databasePath];
+    
+
+    
+    NSLog(@"%@", audioURL);
+    
+    [audioFile getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+        if (error) {
+            NSLog(@"%@",error);
+        }else{
+            
+            NSString *docsDir;
+            NSArray *dirPaths;
+            dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+            docsDir = [dirPaths objectAtIndex:0];
+            NSString *databasePath = [[NSString alloc] initWithString: [docsDir stringByAppendingPathComponent:@"recievedMessage.caf"]];
+            [data writeToFile:databasePath atomically:YES];
+        }
+    }];
+    
+    //[data writeToURL:(NSURL *) audioURL atomically:YES];
     
     
-    
-//    NSURL *audioFileURL = [[NSURL alloc] initWithString:audioFile.url];
+}
+
+//- (void)saveSoundFileToDocuments:(NSData *)soundData fileName:(NSString *)fileName {
+//    NSString *docsDir;
+//    NSArray *dirPaths;
 //    
-//  
-//    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+//    dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+//    docsDir = [dirPaths objectAtIndex:0];
+//    NSString *databasePath = [[NSString alloc] initWithString: [docsDir stringByAppendingPathComponent:@"recievedMessage.caf"]];
+//    [soundData writeToFile:databasePath atomically:YES];
+//}
+
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    
+//    NSArray *dirPaths;
+//    NSString *docsDir;
+//    
+//    dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+//    docsDir = dirPaths[0];
+//    
+//    NSString *soundFilePath = [docsDir stringByAppendingPathComponent:@"recievedMessage.caf"];
+//    
+//    NSURL *soundFileURL = [NSURL fileURLWithPath:soundFilePath];
 //    
 //    NSError *error;
 //    
-//    self.audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:audioFileURL error:&error];
+//    NSLog(@" %@",docsDir);
 //    
+//    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+//    
+//    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:soundFileURL error:&error];
 //    
 //    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error: &error];
 //    
@@ -46,31 +95,24 @@
 //    
 //    _audioPlayer.delegate = self;
 //    
-//    NSLog(@"%@",audioFileURL);
-//    
+//    NSLog(@" %@",soundFileURL);
 //    
 //    [self.audioPlayer play];
     
 }
 
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
-
-
-
-
-
-
-
-
-
