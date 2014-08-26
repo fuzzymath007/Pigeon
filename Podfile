58b867df5991b4934e9d6b91cc6f606d85d9ff0d
fuzzymath007 <mchess@umich.edu>
2014-08-25 22:23:02 -0400

    Added Libraries.

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, "6.0"
+
+target "Pigeon" do
+
+pod 'Parse', '1.2.20'
+pod 'TheAmazingAudioEngine', '1.4.2'
+pod 'GoogleAnalytics-iOS-SDK'
+
+end
+
+target "PigeonTests" do
+
+end
+
---
+++ Pods/Pods-Pigeon-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Bolts
+#define COCOAPODS_POD_AVAILABLE_Bolts
+#define COCOAPODS_VERSION_MAJOR_Bolts 1
+#define COCOAPODS_VERSION_MINOR_Bolts 1
+#define COCOAPODS_VERSION_PATCH_Bolts 2
+
+// Facebook-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_Facebook_iOS_SDK
+#define COCOAPODS_VERSION_MAJOR_Facebook_iOS_SDK 3
+#define COCOAPODS_VERSION_MINOR_Facebook_iOS_SDK 17
+#define COCOAPODS_VERSION_PATCH_Facebook_iOS_SDK 0
+
+// GoogleAnalytics-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_GoogleAnalytics_iOS_SDK
+#define COCOAPODS_VERSION_MAJOR_GoogleAnalytics_iOS_SDK 3
+#define COCOAPODS_VERSION_MINOR_GoogleAnalytics_iOS_SDK 0
+#define COCOAPODS_VERSION_PATCH_GoogleAnalytics_iOS_SDK 9
+
+// Parse
+#define COCOAPODS_POD_AVAILABLE_Parse
+#define COCOAPODS_VERSION_MAJOR_Parse 1
+#define COCOAPODS_VERSION_MINOR_Parse 2
+#define COCOAPODS_VERSION_PATCH_Parse 20
+
+// TheAmazingAudioEngine
+#define COCOAPODS_POD_AVAILABLE_TheAmazingAudioEngine
+#define COCOAPODS_VERSION_MAJOR_TheAmazingAudioEngine 1
+#define COCOAPODS_VERSION_MINOR_TheAmazingAudioEngine 4
+#define COCOAPODS_VERSION_PATCH_TheAmazingAudioEngine 2
+
